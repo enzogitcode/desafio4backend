8ac6ce951266ef0a9b7ae754f65979e7b0873bff
enzogit <enzo_91_10@hotmail.com>
2024-03-26 20:05:17 -0300

    2do commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express();
 const PUERTO = 8080;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto ${PUERTO}`)
 })
-app.get("/", (req, res) => {
-    res.send(`Puerto ${PUERTO} funcionando`)
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const exphbs = require("express-handlebars")
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars')
+app.set("views", './src/views')
+app.use (express.static (".src/public"))
+
+app.get("/", (req, res) => {
+    const usuario= {
+        nombre:"tinky",
+        apellido: "winky"
+
+    }
+    const arrayProductos= ""
+    res.render('index', {usuario, arrayProductos, titulo: "platillita"})
+})
+app.get("/contacto", (req, res) => {
+    res.render("contacto")
+})
