548f07e7434b1e4a5e0a0f75ca92d8633f5be8b4
enzogit <enzo_91_10@hotmail.com>
2024-03-28 04:41:12 -0300

    cambios socket

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const app = express();
 const exphbs = require("express-handlebars");
 const socket = require("socket.io");
+const productsRouter = require("./routes/products-router.js");
+const cartsRouter = require("./routes/carts-router.js");
+const viewsRouter = require("./routes/views-router.js");
 
-const app = express();
 const PUERTO = 8080;
 const httpServer = app.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto ${PUERTO}`)
@@ -10,13 +13,10 @@ const httpServer = app.listen(PUERTO, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const productsRouter = require("./routes/products-router.js");
-const cartsRouter = require("./routes/carts-router.js");
-const viewsRouter = require("./routes/views-router.js");
 
 app.use("/api", productsRouter);
 app.use("/api", cartsRouter);
-//app.use("/", viewsRouter);
+app.use("/", viewsRouter);
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars')
@@ -32,4 +32,19 @@ const productManager = new ProductManager("../src/models/products.json")
 io.on("connection", async (socket) => {
     console.log("un cliente conectado")
 })
+io.on("connection", async (socket) => {
+    console.log("Un cliente conectado");
 
+    socket.emit("productos", await productManager.getProducts());
+
+    socket.on("eliminarProducto", async (id) => {
+        await productManager.deleteProduct(id);
+ 
+        socket.emit("productos", await productManager.getProducts());
+    })
+
+    socket.on("agregarProducto", async (producto) => {
+        await productManager.addProduct(producto);
+        socket.emit("productos", await productManager.getProducts());
+    })
+})
