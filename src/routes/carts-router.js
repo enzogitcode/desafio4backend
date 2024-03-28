b35d530047c11cfdf055d87c1d37d10a9175c543
enzogit <enzo_91_10@hotmail.com>
2024-03-28 06:03:37 -0300

    commit

---
+++ src/routes/carts-router.js
@@ -0,0 +1,43 @@
+const express= require ('express')
+const cartsRouter = express.Router();
+const CartManager= require ('../controllers/cart-manager.js')
+const cartManager = new CartManager('./src/models/carts.json');
+
+cartsRouter.post("/carts", async (req, res) => {
+    try {
+        const nuevoCarrito = await cartManager.crearCarrito();
+        res.json(nuevoCarrito);
+    } catch (error) {
+        res.status(500).json({
+            error: "Error interno del servidor"
+        });
+    }
+})
+
+
+cartsRouter.get("/carts/:cid", async (req, res) => {
+    const cartId = parseInt(req.params.cid)
+    try {
+        const nvoCart = await cartManager.getCartbyId(cartId);
+        res.json(nvoCart.products)
+    } catch (error) {
+        res.status(500).json({ error: "error interno del servidor GET" })
+
+    }
+})
+
+cartsRouter.post("/carts/:cid/product/:pid", async (req, res) => {
+    const cartId = parseInt(req.params.cid)
+    const productId = parseInt(req.params.pid)
+    const quantity = req.body.quantity || 1;
+    try {
+        const updateCart = await cartManager.agregarProductoAlCarrito(cartId, productId, quantity);
+        res.json(updateCart.products);
+    } catch (error) {
+        res.status(500).json({ error: "error interno del servidor Post" })
+
+    }
+})
+
+
+module.exports= cartsRouter;
